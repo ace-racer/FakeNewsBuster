c7ddf02f852ddb35a9771bacbd9bc41c9eb766b9
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2017-05-20 15:00:00 +0530

    Initial code commit

---
+++ businessLogics.js
@@ -0,0 +1,37 @@
+﻿var fakeNewsBuster = {};
+fakeNewsBuster.hyperfeedString = "hyperfeed";
+
+fakeNewsBuster.getAllDivs = function () {
+    var divs = document.getElementsByTagName("div");
+    alert("Div count: " + divs.length);
+}
+
+var divsInPage = fakeNewsBuster.getAllDivs();
+if (divsInPage && divsInPage.length > 0) {
+    for (var i = 0; i < divsInPage.length; i++) {
+        var divId = divsInPage[i].id;
+        if (divId) {
+            if (divId.indexOf(fakeNewsBuster.hyperfeedString) !== -1) {
+                console.log("Found div: " + divId);
+                divsInPage[i].style.backgroundColor = "red";
+            }
+        }
+    }
+}
+
+//var fakeNewsBuster = {};
+//fakeNewsBuster.alexaRankingUrl = "http://data.alexa.com/data?cli=10&dat=snbamz&url=";
+//fakeNewsBuster.checkUrl = function () {
+//    // TODO: Get div from Facebook new feed and find out the URL
+//    var url = "";
+//    if(url)
+//    {
+//        var alexaRequestUrl = fakeNewsBuster.alexaRankingUrl + url;
+//        $.ajax(alexaRequestUrl).
+//            done(function(response){
+
+//    }).fail(function(){
+//    })
+//    }
+//}
+
